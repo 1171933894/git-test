65347eb883283694611a45d4be301793fc06897e
heyuanxin3 <heyuanxin3@jd.com>
2020-05-30 08:44:29 +0800

    dev2 commit

--- jdk-test/src/main/java/Test.java
+++ jdk-test/src/main/java/Test.java
@@ -5,7 +5,8 @@
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println ("hello world!");
-        System.out.println ("hello world!");
+        System.out.println ("123");
+        System.out.println ("234");
+        System.out.println ("345");
     }
 }
