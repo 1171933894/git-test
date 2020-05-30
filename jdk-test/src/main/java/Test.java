c6bff7012971800b600ede00bfc860a746a4b50d
heyuanxin3 <heyuanxin3@jd.com>
2020-05-30 08:15:11 +0800

    dev2 commit

--- jdk-test/src/main/java/Test.java
+++ jdk-test/src/main/java/Test.java
@@ -1,9 +1,10 @@
-package PACKAGE_NAME;
-
 /**
  * @Description:
  * @Author: heyuanxin3
  * @Date: 2020/5/30 7:57
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println ("hello world!");
+    }
 }
