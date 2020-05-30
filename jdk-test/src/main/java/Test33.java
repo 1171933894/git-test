9e449894541a9d16dbff428c3e037b624df343b8
heyuanxin3 <heyuanxin3@jd.com>
2020-05-30 09:33:39 +0800

    dev1 commit

---
+++ jdk-test/src/main/java/Test33.java
@@ -0,0 +1,13 @@
+/**
+ * @Description:
+ * @Author: heyuanxin3
+ * @Date: 2020/5/30 8:02
+ */
+public class Test33 {
+    public static void main(String[] args) {
+        System.out.println ("123");
+        System.out.println ("234");
+        System.out.println ("345");
+        System.out.println ("987");
+    }
+}
