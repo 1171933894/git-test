fcf812606b88d54319e8a5e1ae49b1dd9e32d0a8
heyuanxin3 <heyuanxin3@jd.com>
2020-05-30 08:12:33 +0800

    dev1 commit

--- jdk-test/src/main/java/Test.java
+++ jdk-test/src/main/java/Test.java
@@ -1,9 +1,10 @@
-package PACKAGE_NAME;
-
 /**
  * @Description:
  * @Author: heyuanxin3
  * @Date: 2020/5/30 8:02
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println ("123");
+    }
 }
